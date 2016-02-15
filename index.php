084a996b81fdafb8da175dbb35abb080f9452e06
danixland <danix@danixland.net>
2016-02-15 13:39:44 +0100

    added option to output sass code for current palette

--- index.php
+++ index.php
@@ -8,39 +8,43 @@
         <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,300' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <div id="page">
+        <form id="page" method="post" action="preview.php">
             <header id="masthead">
-                <h1 class="site-title"><a href="">Color Palette viewer</a></h1>
+                <h1 class="site-title"><a href="<?php echo htmlentities( $_SERVER['REQUEST_URI'] ); ?>">Color Palette viewer</a></h1>
+                <input name="preview-palette" type="submit" id="preview-palette" value="preview palette">
             </header>
             <div id="content">
                 <div id="palette">
                     <div id="dark-grey" class="palette-color">
                         <h2 class="usage">main text</h2>
-                        <input class="code" type="text" placeholder="#333335" />
+                        <input name="main-text" class="code" type="text" placeholder="#333335" value="#333335" />
                     </div>
                     <div id="blue-grey" class="palette-color">
                         <h2 class="usage">secondary text</h2>
-                        <input class="code" type="text" placeholder="#2c3646" />
+                        <input name="secondary-text" class="code" type="text" placeholder="#2c3646" value="#2c3646" />
                     </div>
                     <div id="light-grey" class="palette-color">
                         <h2 class="usage">link</h2>
-                        <input class="code" type="text" placeholder="#7c8491" />
+                        <input name="link" class="code" type="text" placeholder="#7c8491" value="#7c8491" />
                     </div>
                     <div id="almost-white" class="palette-color">
                         <h2 class="usage">background</h2>
-                        <input class="code" type="text" placeholder="#eaeaea" />
+                        <input name="background" class="code" type="text" placeholder="#eaeaea" value="#eaeaea" />
                     </div>
                     <div id="crimson-red" class="palette-color">
                         <h2 class="usage">alert</h2>
-                        <input class="code" type="text" placeholder="#8d0011" />
+                        <input name="alert" class="code" type="text" placeholder="#8d0011" value="#8d0011" />
                     </div>
                     <div id="matte-yellow" class="palette-color">
                         <h2 class="usage">accent</h2>
-                        <input class="code" type="text" placeholder="#e8c001" />
+                        <input name="accent" class="code" type="text" placeholder="#e8c001" value="#e8c001" />
                     </div>
+                </div>
+                <div id="sass">
+                    <input name="sass_output" type="submit" id="print-sass" value="sassy">
                 </div>
             </div>
             <footer id="colophon">concept by <a href="http://danixland.net">danix</a></footer>
-        </div>
+        </form>
     </body>
 </html>
--- preview.php
+++ preview.php
@@ -26,6 +26,9 @@
             #mock-page #mock-header h1#mock-title a {
                 color: <?php echo $_POST['accent']; ?>;
             }
+            #mock-page #mock-content span.accent {
+                color: <?php echo $_POST['accent']; ?>;
+            }
             #mock-page #mock-content pre.mock-code {
                 background-color: <?php echo $_POST['link']; ?>;
             }
@@ -45,12 +48,13 @@
                 <h1 class="site-title"><a href="<?php echo htmlentities( dirname($_SERVER['REQUEST_URI']) ); ?>">Color Palette viewer</a></h1>
             </header>
             <div id="content">
+<?php if (isset($_POST['preview-palette'])) : ?>
                 <div id="mock-page">
                     <header id="mock-header">
                         <h1 id="mock-title"><a href="#">This is the title</a></h1>
                     </header>
                     <div id="mock-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus ducimus architecto fuga officiis, asperiores, perferendis? Earum, <a href="#">veritatis</a>, aliquid. Laborum, tempore.</p>
+                        <p>Lorem ipsum dolor <span class="accent">sit amet</span>, consectetur adipisicing elit. Voluptatibus ducimus architecto fuga officiis, asperiores, perferendis? Earum, <a href="#">veritatis</a>, aliquid. Laborum, tempore.</p>
                         <p class="alert">This is an alert. Be careful!!</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, voluptatibus nisi incidunt hic possimus labore, vel quasi, dolorum tenetur quisquam, voluptate unde.</p>
                         <pre class="mock-code">
@@ -64,6 +68,25 @@
                     </div>
                     <footer id="mock-footer">this is a <a href="#">link</a></footer>
                 </div>
+<?php elseif (isset($_POST['sass_output'])) :?>
+                <div id="sass-output">
+                    <p>you can copy this text and save it as a partial in your sass project as <code>_colors.scss</code> and then import it in your main scss file with:</p>
+                    <p><code>@import "colors";</code></p>
+                    <p>
+<pre>
+// scss colors created using "color-palette-viewer by danix"
+// https://github.com/danixland/palette-viewer
+
+$background:    <?php echo $_POST['background'] . "\n"; ?>
+$main_text:     <?php echo $_POST['main-text'] . "\n"; ?>
+$alt_text:      <?php echo $_POST['secondary-text'] . "\n"; ?>
+$link:          <?php echo $_POST['link'] . "\n"; ?>
+$alert:         <?php echo $_POST['alert'] . "\n"; ?>
+$accent:        <?php echo $_POST['accent'] . "\n"; ?>
+</pre>
+                    </p>
+                </div>
+<?php endif; ?>
             </div>
             <footer id="colophon">concept by <a href="http://danixland.net">danix</a></footer>
         </div>
